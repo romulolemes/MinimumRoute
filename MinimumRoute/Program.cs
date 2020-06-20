749fcbe7aa5dd94b969d2f6f0014ef23aa667dcd
Romulo Rocha Lemes <rrlemes1@latam.stefanini.com>
2020-06-20 13:29:32 -0300

    Add class runner

--- MinimumRoute/Program.cs
+++ MinimumRoute/Program.cs
@@ -6,11 +6,9 @@ using MinimumRoute.Search;
 using MinimumRoute.Search.Dijkstra;
 using MinimumRoute.Serialization;
 using MinimumRoute.Service;
-using MinimumRoute.ViewModel;
 using Serilog;
 using Serilog.Extensions.Logging;
 using System;
-using System.Linq;
 
 namespace MinimumRoute
 {
@@ -28,34 +26,17 @@ namespace MinimumRoute
 
             try
             {
-                Log.Debug("Starting application");
+                Log.Information("Starting application");
                 RegisterServices();
 
-                var fileService = _serviceProvider.GetService<FileService>();
-                var routeService = _serviceProvider.GetService<RouteService>();
-                var finder = _serviceProvider.GetService<IShortestPathFinder>();
-                var cityService = _serviceProvider.GetService<CityService>();
-                var textSerializer = _serviceProvider.GetService<TextSerializer>();
-
-                var contentRoutes = fileService.ReadFile("./trechos.txt");
-                var contentOrders = fileService.ReadFile("./encomendas.txt");
-
-                var routesViewModel = textSerializer.SerializeList<RouteViewModel>(contentRoutes);
-                var ordersViewModel = textSerializer.SerializeList<OrderViewModel>(contentOrders);
-
-                routeService.CreateListRoutes(routesViewModel);
-                var paths = ordersViewModel.Select(order => finder.FindShortestPath(cityService.FindByCode(order.CityOrigin), cityService.FindByCode(order.CityDestination)));
-
-                var textPaths = textSerializer.DeserializeList(paths);
-                fileService.WriteFile("./rotas.txt", textPaths);
-
-                Log.Debug("{paths}", textPaths);
+                var runner = _serviceProvider.GetService<Runner>();
+                runner.Run();
 
                 Log.Information("Finished application");
             }
             catch (Exception ex)
             {
-                Log.Fatal(ex, "Application start-up failed");
+                Log.Fatal(ex, "Application failed");
             }
             finally
             {
@@ -68,6 +49,7 @@ namespace MinimumRoute
             _serviceProvider = new ServiceCollection()
                 .AddLogging()
                 .AddSingleton(services => (ILoggerFactory)new SerilogLoggerFactory(Log.Logger, true))
+                .AddSingleton<Runner>()
                 .AddSingleton<Context>()
 
                 .AddTransient<CityRepository>()
---
+++ MinimumRoute/Runner.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Logging;
+using MinimumRoute.Search;
+using MinimumRoute.Serialization;
+using MinimumRoute.Service;
+using MinimumRoute.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MinimumRoute
+{
+    public class Runner
+    {
+        protected FileService _fileService;
+        protected RouteService _routeService;
+        protected IShortestPathFinder _shortestPathFinder;
+        protected CityService _cityService;
+        protected TextSerializer _textSerializer;
+        protected ILogger _logger;
+
+        public Runner(FileService fileService,
+            RouteService routeService,
+            IShortestPathFinder shortestPathFinder,
+            CityService cityService,
+            TextSerializer textSerializer,
+            ILogger<Runner> logger)
+        {
+            _fileService = fileService ?? throw new ArgumentNullException(nameof(fileService));
+            _routeService = routeService ?? throw new ArgumentNullException(nameof(routeService));
+            _shortestPathFinder = shortestPathFinder ?? throw new ArgumentNullException(nameof(shortestPathFinder));
+            _cityService = cityService ?? throw new ArgumentNullException(nameof(cityService));
+            _textSerializer = textSerializer ?? throw new ArgumentNullException(nameof(textSerializer));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+
+        public void Run()
+        {
+            List<RouteViewModel> routesViewModel = ReadAndSerializer<RouteViewModel>("./trechos.txt");
+            List<OrderViewModel> ordersViewModel = ReadAndSerializer<OrderViewModel>("./encomendas.txt");
+
+            _routeService.CreateListRoutes(routesViewModel);
+
+            var paths = ordersViewModel.Select(order =>
+            {
+                var cityOrigin = _cityService.FindByCode(order.CityOrigin);
+                var cityDestination = _cityService.FindByCode(order.CityDestination);
+                return _shortestPathFinder.FindShortestPath(cityOrigin, cityDestination);
+            });
+
+            var textPaths = _textSerializer.DeserializeList(paths);
+            _fileService.WriteFile("./rotas.txt", textPaths);
+
+            _logger.LogDebug("{paths}", textPaths);
+        }
+
+        private List<T> ReadAndSerializer<T>(string path)
+        {
+            string contentRoutes = _fileService.ReadFile(path);
+            return _textSerializer.SerializeList<T>(contentRoutes);
+        }
+    }
+}
